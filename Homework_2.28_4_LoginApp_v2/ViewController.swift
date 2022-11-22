a7b555feed739730de98f071ed3cbc10ab49a301
Ravil <ravilgab@yahoo.com>
2022-11-22 15:15:51 +0600

    Project created

---
+++ Homework_2.28_4_LoginApp_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.28_4_LoginApp_v2
+//
+//  Created by Ravil on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
